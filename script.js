336b8908b59fc153e3d31ad3dc79f1b23f10199d
BraulioEstrada-Ksquare <Braulio.Estrada@itksquare.edu.mx>
2023-03-24 15:31:41 -0600

    hechas US 1,2,3,6,8,9

---
+++ script.js
@@ -0,0 +1,30 @@
+const quote = document.querySelector(".quote");
+const background = document.querySelector(".background");
+
+function getCouncil(){
+    const advices = fetch("https://api.adviceslip.com/advice");
+    advices.then(res => res.json())
+        .then(response => {
+            quote.innerText = `"${response.slip.advice}"`;
+        })
+}
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function getRandomColor () {
+    const r = getRandomNumber(255);
+    const g = getRandomNumber(255);
+    const b = getRandomNumber(255);
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+
+  function getCouncilAndRandomColorBackground(){
+    getCouncil();
+    setTimeout(background.style.backgroundColor = getRandomColor(),2000)
+    
+
+  }
+
+getCouncil();
